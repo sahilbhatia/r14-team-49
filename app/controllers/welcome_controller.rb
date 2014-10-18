d56e04ddfccd7033e6dde5714205d3954122c39d
yogesh <yogesh@joshsoftware.com>
2014-10-18 19:22:25 +0530

    adding twitter_api lib and fetch_tweet action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
-  def index
+  def fetch_tweets
+    @result_set = TwitterApi.fetch_tweets_for(safe_params[:query], :country => safe_params[:country]).to_json
+    render :index
+  end
+
+  private
+
+  def safe_params
+    params.fetch(:criteria, {}).permit(:query, :country)
   end
 end
