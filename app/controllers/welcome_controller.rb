9180c62984abd2406bff1a80f64dbfdd0f8303ef
Sahil Bhatia <sahil@joshsoftware.com>
2014-10-18 18:32:02 +0530

    added code to return country code from geolocation on page load

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def get_country_code
+    if params[:coordinates].present?
+      country_code = Geocoder.search(params[:coordinates]).last.address_components.last['short_name'].downcase
+      render json: { country_code: country_code } and return
+    else
+      render json: { error: 'Invalid or no coordinates...' } and return
+    end
+  end
 end
