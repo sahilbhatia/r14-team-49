fbdec5929862031dbf1d044157a5e023f396aa1b
Shweta <shweta@joshsoftware.com>
2014-10-19 18:06:03 +0530

    10 countries added

--- config/initializers/countries.rb
+++ config/initializers/countries.rb
@@ -1,1 +1,1 @@
-COUNTRIES = [['India','in'], ['U.S.A','us']]
+COUNTRIES = [['Australia','au'],['Brazil','br'],['Canada','ca'],['China','cn'],['France','fr'],['India','in'],['Japan','jp'],['New Zealand','nz'],['Singapore','sg'],['U.S.A','us']]
